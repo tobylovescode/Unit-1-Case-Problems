dac3a150ab20b5021070897c13cdb56628e778b2
tobylovescode <awilliams1893@gmail.com>
2026-08-24 00:53:27 +0000

    Upload Unit-1-Case-Problem

---
+++ YummyMotto2.java
@@ -0,0 +1,9 @@
+public class YummyMotto2 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("***********************************************");
+        System.out.println("* Yummy makes the food that makes it a party. *");
+        System.out.println("***********************************************");
+    }
+}
